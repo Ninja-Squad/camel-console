e579a2c7847e20abe159c326ee3a836bfc528f4c
JB Nizet <jnizet@gmail.com>
2012-10-26 18:13:12 +0200

    re-add files with correct case

---
+++ console-web/src/main/webapp/js/models/Route.js
@@ -0,0 +1,19 @@
+define(['backbone'], function(Backbone) {
+    var Route = Backbone.Model.extend({
+        defaults: {
+            name: 'unnamed',
+            messageCount: 0,
+            successCount: 0,
+            failureCount: 0,
+            successRate: 0,
+            averageTime: 0,
+            minimumTime: 0,
+            maximumTime: 0
+        },
+        initialize: function() {
+            this.set('successRate', Math.floor(this.get('successCount') * 100 / this.get('messageCount')));
+        }
+    });
+    
+    return Route;
+});
