dc7f67eb5e9b252bf60021e992a19307b3b7ae58
cexbrayat <cexbrayat@gmail.com>
2012-10-31 20:43:28 +0100

    useless logger

--- console-web/src/main/java/com/ninja_squad/console/WebAppConfigurer.java
+++ console-web/src/main/java/com/ninja_squad/console/WebAppConfigurer.java
@@ -1,13 +1,11 @@
 package com.ninja_squad.console;
 
 import com.ninja_squad.console.subscriber.NotificationSubscriber;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@Slf4j
 @Configuration
 @EnableMongoRepositories(value = "com.ninja_squad.console.repository", repositoryImplementationPostfix = "CustomDefault")
 @ImportResource({"classpath*:resthubContext.xml", "classpath*:applicationContext.xml"})
